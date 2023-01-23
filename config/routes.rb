d65c0e2548b8f6fcf2ef78a985f4e13a266fa23d
toukaeru <senkoutoburakki@outlook.jp>
2023-01-23 07:18:26 +0000

    [Add]44-27

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
   root to: 'homes#top'
   devise_for :users
   get 'homes/about' => 'homes#about'
+  resources :books, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
